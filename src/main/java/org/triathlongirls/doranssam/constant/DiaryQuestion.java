bf928bff959e89cd40c96bc6bc061247fa5f5b08
WHITE-7\CAU <safg0421@gmail.com>
2022-06-09 03:44:43 +0900

    fix: 2단계에 줄바꿈

--- src/main/java/org/triathlongirls/doranssam/constant/DiaryQuestion.java
+++ src/main/java/org/triathlongirls/doranssam/constant/DiaryQuestion.java
@@ -12,16 +12,16 @@ import java.util.Random;
 public class DiaryQuestion {
 
     private static final List<String> COOKING_DIARY2 = List.of(
-            "오늘 어떤 요리를 했나요? 요리 과정을 설명해주세요. 완성된 요리는 맛있었나요? 다음에 요리할 음식은 무엇인가요?"
+            "오늘 어떤 요리를 했나요?\n요리 과정을 설명해주세요.\n완성된 요리는 맛있었나요?\n다음에 요리할 음식은 무엇인가요?"
     );
 
     private static final List<List<String>> COOKING_DIARY1 = List.of(
             List.of(
-                    "오늘 어떤 요리를 했나요?\n",
-                    "요리를 하게 된 이유가 무엇인가요?\n",
-                    "요리 과정을 설명해주세요. 어떻게 요리했나요?\n",
-                    "요리는 어떻게 완성되었나요?\n",
-                    "다음에는 어떤 요리를 하고 싶나요?\n"
+                    "오늘 어떤 요리를 했나요?",
+                    "요리를 하게 된 이유가 무엇인가요?",
+                    "요리 과정을 설명해주세요. 어떻게 요리했나요?",
+                    "요리는 어떻게 완성되었나요?",
+                    "다음에는 어떤 요리를 하고 싶나요?"
             )
     );
 
