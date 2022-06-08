71acb2ecfecf446e5f7e21d6c6743a9e04621943
WHITE-7\CAU <safg0421@gmail.com>
2022-06-09 04:11:59 +0900

    fix: 이달의 일기 현재날짜로 조회

--- src/main/java/org/triathlongirls/doranssam/repository/DiaryRepository.java
+++ src/main/java/org/triathlongirls/doranssam/repository/DiaryRepository.java
@@ -23,7 +23,9 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     @Query(value = "select *  " +
             "from diaries d " +
-            "where year(d.date) = ?1 and month(d.date) =?2 and d.user_id =?3 " +
+            "where year(d.date) = year(CURRENT_DATE ) " +
+            "and month(d.date) = month(CURRENT_DATE ) " +
+            "and d.user_id =?1 " +
             "order by  d.date"
             , nativeQuery = true)
     List<Diary> findDiaryThisMonth(Long userId);
