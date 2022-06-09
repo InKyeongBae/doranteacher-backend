48a52df65e39990204b68f354df39495709c35a1
InKyeongBae <inkkyung@gmail.com>
2022-06-09 14:48:25 +0900

    Feat : Flask Api result string parsing test

--- src/main/java/org/triathlongirls/doranssam/service/diaries/FlaskService.java
+++ src/main/java/org/triathlongirls/doranssam/service/diaries/FlaskService.java
@@ -1,16 +1,21 @@
 package org.triathlongirls.doranssam.service.diaries;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.nd4j.linalg.util.LinkedMultiValueMap;
 import org.nd4j.linalg.util.MultiValueMap;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.triathlongirls.doranssam.dto.FlaskResponseDto;
 
 @Service
 public class FlaskService {
 
-    public HttpEntity<String> requestToFlask(String text) {
+    public FlaskResponseDto requestToFlask(String text) throws JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://localhost:4000/recommend";
 
@@ -18,13 +23,19 @@ public class FlaskService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("text", text);
         HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         // Combine Message
         HttpEntity<?> requestMessage = new HttpEntity<>(body, httpHeaders);
 
         // Request and getResponse
         HttpEntity<String> response = restTemplate.postForEntity(url, requestMessage, String.class);
 
-        return response;
+        // Response Body 파싱
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+        FlaskResponseDto dto = objectMapper.readValue(response.getBody(), FlaskResponseDto.class);
+
+        return dto;
     }
 
 }
