6e6fb8e520d0f100059283a5758e7488803404f6
InKyeongBae <inkkyung@gmail.com>
2022-06-09 17:53:44 +0900

    Feat : 수정 테스트 2

--- src/main/java/org/triathlongirls/doranssam/service/diaries/FlaskService.java
+++ src/main/java/org/triathlongirls/doranssam/service/diaries/FlaskService.java
@@ -3,11 +3,9 @@ package org.triathlongirls.doranssam.service.diaries;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.*;
 import org.nd4j.linalg.util.LinkedMultiValueMap;
 import org.nd4j.linalg.util.MultiValueMap;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.triathlongirls.doranssam.dto.FlaskResponseDto;
@@ -27,13 +25,18 @@ public class FlaskService {
         // Combine Message
         HttpEntity<?> requestMessage = new HttpEntity<>(body, httpHeaders);
 
-        // Request and getResponse
-        HttpEntity<String> response = restTemplate.postForEntity(url, requestMessage, String.class);
+//        // Request and getResponse
+//        HttpEntity<String> response = restTemplate.postForEntity(url, requestMessage, String.class);
+//
+
+        RestTemplate restTemplate2 = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate2.exchange("http://localhost:4000/recommend", HttpMethod.POST,
+                requestMessage, String.class);
 
         // Response Body 파싱
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
-        FlaskResponseDto dto = objectMapper.readValue(response.getBody(), FlaskResponseDto.class);
+        FlaskResponseDto dto = objectMapper.readValue(responseEntity.getBody(), FlaskResponseDto.class);
 
         return dto;
     }
