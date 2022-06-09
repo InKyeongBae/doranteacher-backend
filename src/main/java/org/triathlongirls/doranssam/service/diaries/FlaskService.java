beacd1284cc5f74184626b6f66093ffd80e67aab
InKyeongBae <inkkyung@gmail.com>
2022-06-09 19:02:36 +0900

    Feat : port replace

--- src/main/java/org/triathlongirls/doranssam/service/diaries/FlaskService.java
+++ src/main/java/org/triathlongirls/doranssam/service/diaries/FlaskService.java
@@ -16,7 +16,7 @@ public class FlaskService {
 
     public String requestToFlask(String text) throws JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://127.0.0.1:8080/recommend";
+        String url = "http://192.168.50.33:8080/recommend";
 
         // Body set
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
